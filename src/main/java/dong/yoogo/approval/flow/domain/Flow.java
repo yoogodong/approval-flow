18f91c19e2f8e4e7815b16b9809a4a9a8ff9f3f3
yoogodong <yoogodong@gmail.com>
2021-05-09 19:10:30 +0800

    简化 Flow / FlowNode

--- src/main/java/dong/yoogo/approval/flow/domain/Flow.java
+++ src/main/java/dong/yoogo/approval/flow/domain/Flow.java
@@ -1,29 +1,20 @@
 package dong.yoogo.approval.flow.domain;
 
-import dong.yoogo.approval.flow.constant.ApproverResult;
-import dong.yoogo.approval.flow.constant.ErrorCode;
 import dong.yoogo.approval.flow.constant.FlowStatus;
 import dong.yoogo.approval.flow.constant.NodeStatus;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONException;
-import com.alibaba.fastjson.JSONObject;
-import lombok.*;
+import dong.yoogo.approval.flow.service.CreateFlowIN;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-
-import javax.persistence.*;
-import javax.validation.constraints.Digits;
-
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.math.BigDecimal;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * 审批流程
@@ -41,38 +32,12 @@ import java.util.stream.Collectors;
 @ToString
 @Slf4j
 public class Flow {
-
-
     /**
      * 流水号
      */
     @Id
     @Column(name = "TRANS_NO")
     private String transactionNo;
-    /**
-     * 制单人 userNO
-     */
-    @NonNull
-    private String creatorNo;
-
-    /**
-     * 客户
-     */
-    @NonNull
-    @Column(name = "CUSTOMER_NO")
-    private Long customerNo;
-    /**
-     * 渠道
-     */
-    private String channelId;
-    /**
-     * 账户
-     */
-    private String accountNo;
-    /**
-     * 功能码，用以按交易类型分组
-     */
-    private String functionCode;
 
     /**
      * 流程创建时间， 用以排序, 相当于制单时间
@@ -84,32 +49,13 @@ public class Flow {
      * 当前审批节点的索引
      */
     private int currentLevelIndex = 0;
-    /**
-     * 交易金额
-     */
-    @Digits(integer = 19, fraction = 2)
-    @Column(name = "TRANS_AMOUNT")
-    private BigDecimal transactionAmount;
+
 
     /**
      * 流程状态
      */
     @Enumerated(EnumType.STRING)
     private FlowStatus flowStatus;
-
-    /**
-     * 当前审批节点的所有审核人列表
-     */
-    @ElementCollection()
-    @CollectionTable(name = "TB_OC_APR_FLOW_APPROVER", joinColumns = {@JoinColumn(name = "FLOW_ID")})
-    @Column(name = "APPROVER_NO")
-    private List<String> approverList;
-    /**
-     * 已审批/驳回过的人员列表
-     */
-    @ElementCollection()
-    @CollectionTable(name = "TB_OC_APR_FLOW_HANDLED_APPROVE", joinColumns = {@JoinColumn(name = "FLOW_ID")})
-    private List<HandledApprover> handledApproverList = new ArrayList<>();
 
 
     /**
@@ -119,157 +65,27 @@ public class Flow {
     @OrderColumn
     private List<FlowNode> nodeList = new ArrayList<>();
 
-    /**
-     * 交易的其他数据,被保存成一个 json 串
-     */
-    @Column(name = "EXTRA_DATA", length = 4000)
-    private String extraData;
-
-    @Version
-    @Column(name = "VERSION")
-    private long version;
-
-    /**
-     * 最后一个复核人, 查询落地复核列表时需要, 为了性能考虑, 增加这个字段而不是从复核人列表中取
-     */
-    @Column(name = "LAST_APPROVER")
-    private String lastApprover;
-
-
-    @Embedded
-    @Getter(AccessLevel.PRIVATE)
-    private LandingAppoval landingAppoval;
-
-    /**
-     * 交易错误码
-     */
-    @Column(name = "RET_CODE")
-    private String retCodeFromTransaction;
-    /**
-     * 交易错误信息
-     */
-    @Column(name = "RET_MSG")
-    private String transactionMessage;
-
-    /**
-     * 主账户
-     */
-    @Column(name = "PARENT_ACC_NO")
-    private String parentAccountNo;
-
-    /**
-     * 复核完成后的回调url，应是完整路径（包含http://）
-     */
-    @Column(name = "CALL_BACK_URL")
-    private String callBackUrl;
-    
-	/* 创建时间，用于表分区 */
-	@Column(updatable = false, name = "CREATE_DATE")
-	@CreatedDate
-	private Date createDate;
-	
-	
-	/* 最后复核时间 */
-	@Column(name = "LAST_APPROVAL_DATE")
-	private LocalDateTime lastApprovalDate;
 
 
     public Flow(CreateFlowIN in) {
-        log.debug("初始化一个复核流程:参数={}", in);
-        transactionNo = in.getTransactionNo();
-        customerNo = in.getCustomerNo();
-        channelId = in.getChannelId();
-        accountNo = in.getAccountNo();
-        functionCode = in.getFunctionCode();
-        creatorNo = in.getCreatorNo();
-        transactionAmount = in.getAmount();
-        createTime = LocalDateTime.now();
-        flowStatus = FlowStatus.WAIT_FOR_REVIEW;
-        parentAccountNo = in.getMainAccountNo();
-        extraData = in.getExtraData();
-        callBackUrl = in.getCallBackUrl();
-        landingAppoval = new LandingAppoval(in.getDepartmentNo(), in.getLandingLimit());
+
     }
 
 
-    /**
-     * 将用户定义过的审批模式阶段列表设置进来
-     */
-    public void useSpecifiedFlow(List<ApprovalNodeDTO> modelNodeList) {
-        log.debug("采用这个审批流程:{}", modelNodeList);
-        for (ApprovalNodeDTO modelNode : modelNodeList) {
-            nodeList.add(transfer(modelNode));
-        }
-        updateApproverList();
-    }
-
-    private FlowNode transfer(ApprovalNodeDTO modelNode) {
-        FlowNode flowNode = new FlowNode();
-        flowNode.setTransactionAmount(transactionAmount);
-
-        flowNode.setMinApprovalCount(modelNode.getApprovalCount());
-
-        List<Approver> approvers = modelNode.getApprovalUsers().stream().map(x -> {
-            Long userNo = x.getUserNo();
-            BigDecimal minLimit = x.getMinLimit();
-            BigDecimal maxLimit = x.getMaxLimit();
-            BigDecimal maxOutLimit = x.getMaxOutLimit();
-            return new Approver(userNo.toString(), minLimit, maxLimit, maxOutLimit);
-        }).collect(Collectors.toList());
-        flowNode.setDayOutAmount(modelNode.getDayOutLimit());
-        flowNode.setApproverList(approvers);
-        log.debug("将审批模式节点{},转化为流程节点{}", modelNode, flowNode);
-        return flowNode;
-    }
 
 
-    private void updateApproverList() {
-        log.info("即将更新审批人列表,当前{}", approverList);
-        if (nodeList.size() > 0) {
-            FlowNode flowNode = nodeList.get(currentLevelIndex);
-            approverList = flowNode.getVisibleApproverIds();
-            log.info("已经更新审批人列表,当前{}", approverList);
-        }
-    }
 
-    /**
-     * 查询对指定的复核员来说, 这是不是最后一笔复核
-     */
-    public boolean isLastDeal(String approverId) {
-        log.info("查询经{}复核后,流程是否就完成了", approverId);
-        checkValidResponse(approverId);
-        
-        // 流程节点总数小于需要操作的节点，提示'该笔交易已提交，请勿重复操作'.(需要操作的节点下标从0开始，所以需要加1)
-        if(null == nodeList || (nodeList.size() < currentLevelIndex + 1)) {
-        	throw BusinessUtils.createBusinessException(ErrorCode.HEAD + ErrorCode.ALREADY_SUBMIT);
-        }
-        NodeStatus nodeStatus = nodeList.get(currentLevelIndex).preApproval(approverId);
-        boolean nodeEnd = NodeStatus.END_NODE == nodeStatus;
-        boolean lastLevel = nodeList.size() == currentLevelIndex + 1;
-        boolean deal = nodeStatus == NodeStatus.END_FLOW;
 
-        return nodeEnd && lastLevel || deal;
-    }
 
     /**
      * 复核通过
      *
      * @return 流程是否完成
      */
-    public boolean approve(String approverId, BigDecimal dayLimitUsed,Boolean isLandingAppoval) {
-        log.info("复核同意:approverId={},transactionNo={}", approverId, this.getTransactionNo());
-        // 设置最后复核时间
-        lastApprovalDate = LocalDateTime.now();
-        
-        checkValidResponse(approverId);
-        
-        // 流程节点总数小于需要操作的节点，提示'该笔交易已提交，请勿重复操作'.(需要操作的节点下标从0开始，所以需要加1)
-		if(null == nodeList || (nodeList.size() < currentLevelIndex + 1)) {
-			throw BusinessUtils.createBusinessException(ErrorCode.HEAD + ErrorCode.ALREADY_SUBMIT);
-		}
-        
+    public boolean approve() {
+        log.info("同意:transactionNo={}", this.getTransactionNo());
         //复核，交给节点控制流程。返回（节点完成、交易完成、继续） 三种状态,记录复核结果（通过、驳回、缺省）和 复核时间 到操作员
-        NodeStatus nodeStatus = nodeList.get(currentLevelIndex).approve(approverId, dayLimitUsed);
+        NodeStatus nodeStatus = nodeList.get(currentLevelIndex).approve();
         switch (nodeStatus) {
             case END_FLOW:
                 flowStatus = FlowStatus.COMPLETED;
@@ -281,95 +97,22 @@ public class Flow {
                 flowStatus = FlowStatus.IN_PROCESS;
         }
 
-        updateApprover(approverId, ApproverResult.APPROVED, null);
-
-        //白名单 校验落地复核 true：不在白名单，false：在白名单
-        log.info("落地复核是否在白名单："+isLandingAppoval + "----true：不在白名单，false：在白名单");
-        if(isLandingAppoval) {
-			if (null != landingAppoval && flowStatus == FlowStatus.COMPLETED
-	        		&& landingAppoval.needLandingApproval(transactionAmount)) {
-	            flowStatus = FlowStatus.WAIT_FOR_LANDING_REVIEW;
-	        }
-		}
         return flowStatus == FlowStatus.COMPLETED;
     }
 
-    private void updateApprover(String approverId, ApproverResult result, String rejectReason) {
-        //把当前复核员从当前复核人列表中移动到已复核人员列表中
-        approverList.remove(approverId);
-        lastApprover = approverId;
-        //        添加复核员到已响应的复核员列表中
-        HandledApprover handledApprover = new HandledApprover(approverId, LocalDateTime.now(), result);
-        handledApprover.setRejectReason(rejectReason);
-        setNoBusiField(handledApprover);
-        handledApproverList.add(handledApprover);
-    }
 
-    /**
-     * 设置非业务字段
-     *
-     * @param handledApprover
-     */
-    private void setNoBusiField(HandledApprover handledApprover) {
-        JSONObject parseObject = null;
-        try {
-            parseObject = JSON.parseObject(CurrentHeaderHolder.getCurrentTellerID());
-        } catch (JSONException e) {
-            log.warn("无法解析 tellerId , 实际值为{}", CurrentHeaderHolder.getCurrentTellerID());
-            return;
-        }
-        String clientIp = (String) parseObject.get("clientIp");
-        Object oriConsumer = CurrentHeaderHolder.getCurrentZyHeader().get("oriConsumer");
-        if (null == oriConsumer) {
-            log.error("无法获取复核渠道");
-        } else {
-            handledApprover.setChannelId(oriConsumer.toString());
-        }
-        handledApprover.setClientIp(clientIp);
-        try {
-            String hostName = InetAddress.getLocalHost().getHostName();
-            String hostAddress = InetAddress.getLocalHost().getHostAddress();
-            handledApprover.setTranServiceName(hostName);
-            handledApprover.setTranServiceIp(hostAddress);
-        } catch (UnknownHostException e) {
-            log.error("无法获取当前服务器名称或IP", e);
-        }
 
-    }
 
     /**
      * 复核驳回
      */
     public void reject(String approverId, String rejectReason) {
         log.info("复核驳回:approverId={},,transactionNo={},驳回原因={}", approverId, this.getTransactionNo(), rejectReason);
-        // 设置最后复核时间
-        lastApprovalDate = LocalDateTime.now();
-        
-        checkValidResponse(approverId);
-		
-        // 流程节点总数小于需要操作的节点，提示'该笔交易已提交，请勿重复操作'.(需要操作的节点下标从0开始，所以需要加1)
-		if(null == nodeList || (nodeList.size() < currentLevelIndex + 1)) {
-			throw BusinessUtils.createBusinessException(ErrorCode.HEAD + ErrorCode.ALREADY_SUBMIT);
-		}
         nodeList.get(currentLevelIndex).reject();
         flowStatus = FlowStatus.REJECT;
-        //把当前复核员从当前复核人列表中移动到已相应人员列表中
-        updateApprover(approverId, ApproverResult.REJECT, rejectReason);
     }
 
 
-    private void checkValidResponse(String approverId) {
-        if (!(flowStatus == FlowStatus.WAIT_FOR_REVIEW || flowStatus == FlowStatus.IN_PROCESS)) { //不可复核
-            log.warn("当前流程的状态处于 {},不可以复核", this.flowStatus);
-            throw BusinessUtils.createBusinessException(ErrorCode.HEAD + ErrorCode.FLOW_ENDED, flowStatus);
-        } else if (!approverList.contains(approverId)) {  //必须在复核员列表中
-            log.warn("复核人{},不在列表中{}", approverId, approverList);
-            throw BusinessUtils.createBusinessException(ErrorCode.HEAD + ErrorCode.NOT_ALLOWED);
-        } else if (approverId.equals(creatorNo)) {   //检查是不是此复核员同时是制单员
-            log.warn("制单员企图自己复核{}", approverId);
-            throw BusinessUtils.createBusinessException(ErrorCode.HEAD + ErrorCode.SELF_APPROVAL);
-        }
-    }
 
     private void endNode() {
         if (nodeList.size() == currentLevelIndex + 1) {
@@ -379,97 +122,7 @@ public class Flow {
             log.info("进入第{}级别复核", currentLevelIndex + 2);
             currentLevelIndex++;
             flowStatus = FlowStatus.IN_PROCESS;
-
-            updateApproverList();
         }
     }
 
-
-    /**
-     * 零级复核
-     */
-    public void zeroNode() {
-        if (landingAppoval.needLandingApproval(transactionAmount))
-            flowStatus = FlowStatus.WAIT_FOR_LANDING_REVIEW;
-        else
-            flowStatus = FlowStatus.COMPLETED;
-    }
-
-    /**
-     * 交易回调更新状态
-     *
-     * @param transactionResult
-     * @param code              核心错误码
-     * @param message           错误消息
-     */
-    public void transactionResult(FlowStatus transactionResult, String code, String message) {
-        log.info("交易更新状态到流程: trasnaction result: {}, code:{}, message:{}", transactionResult, code, message);
-        flowStatus = transactionResult;
-        this.retCodeFromTransaction = code;
-        this.transactionMessage = message;
-    }
-
-    /**
-     * 落地复核同意
-     */
-    public void landingApprove(String tellerId, String tellerName) {
-        landingAppoval.approve(tellerId, tellerName);
-        flowStatus = FlowStatus.COMPLETED;
-    }
-
-    /**
-     * 落地复核驳回
-     */
-    public void landingReject(String tellerId, String tellerName, String rejectReason) {
-        landingAppoval.reject(tellerId, tellerName, rejectReason);
-        flowStatus = FlowStatus.REJECT;
-    }
-
-    /**
-     * 获得驳回原因,要么是复核驳回,要么是落地复核驳回
-     */
-    public String getRejectReason() {
-        if (!flowStatus.equals(FlowStatus.REJECT))
-            return null;
-        String rejectReason = landingAppoval.getRejectReason();
-        if (StringUtil.isEmpty(rejectReason)) {
-            rejectReason = handledApproverList.stream().filter(x -> x.getResult() == ApproverResult.REJECT)
-                    .map(x -> x.getRejectReason()).findFirst().orElse(null);
-        }
-        return rejectReason;
-    }
-
-    /**
-     * 为了获得当前复核员的复核时间:从复核员列表中取出当前复核员,获取其时间
-     */
-    public LocalDateTime getHandledTimeOf(String approverId) {
-        if (approverId == null)
-            return null;
-        return handledApproverList.stream()
-                .filter(x -> x.getUserNo().equals(approverId))
-                .map(x -> x.getHandleTime())
-                .findFirst().orElse(null);
-    }
-
-    /**
-     * 是否账户相关
-     */
-    public boolean isAccountOf() {
-        return accountNo != null;
-    }
-    
-	public boolean conatainUserNos(List<UserModelFlowDTO> userNos) {
-		//需要从当前节点之后的节点开始查找，即所有未处理的节点
-		for (int i = currentLevelIndex; i < nodeList.size(); i++) {
-			FlowNode flowNode = nodeList.get(i);
-			List<Long> filterUserNos = new ArrayList<Long>();
-			//需要将该操作员列表中的包含该交易渠道的操作员过滤出来，来进行操作员比较，如果渠道为空，则说明是注销操作员，也需要进行比较
-			userNos.stream().filter(x -> null == x.getChannelIds() || x.getChannelIds().isEmpty()
-					|| x.getChannelIds().contains(channelId)).forEach(x -> filterUserNos.add(x.getUserNo()));
-			if(flowNode.containUserNos(filterUserNos)) {
-				return true;
-			}
-		}
-		return false;
-	}
 }
--- src/main/java/dong/yoogo/approval/flow/domain/FlowNode.java
+++ src/main/java/dong/yoogo/approval/flow/domain/FlowNode.java
@@ -7,12 +7,14 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.persistence.*;
-import javax.validation.constraints.Digits;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Slf4j
 @Entity
@@ -29,21 +31,6 @@ public class FlowNode {
     private Long id;
 
     /**
-     * 交易额度
-     */
-    @Setter
-    @Digits(integer = 19, fraction = 2)
-    @Column(name = "TRANS_AMOUNT")
-    private BigDecimal transactionAmount;
-    
-    /**
-     * 日累计对外支付额
-     */
-    @Setter
-    @Column(name = "DAY_OUT_AMOUNT")
-    private BigDecimal dayOutAmount;
-
-    /**
      * 节点状态
      */
     @Enumerated(EnumType.STRING)
@@ -56,37 +43,13 @@ public class FlowNode {
 
     private int approvedCount = 0;
 
-    @Setter
-    @ElementCollection
-    @CollectionTable(name = "TB_OC_APR_FLOWNODE_APPROVER")
-    private List<Approver> approverList = new ArrayList<>();
-
-
-    public List<String> getVisibleApproverIds() {
-        List<String> visibleApprover = approverList.stream()
-                .filter(x -> x.isVisible(transactionAmount))
-                .map(x -> x.getUserNo()).collect(Collectors.toList());
-        log.debug("获得可以复核的人员列表:{}", visibleApprover);
-        return visibleApprover;
-    }
 
 
     /**
      * 复核通过
-     * @param dayOutLimit 
      */
-    public NodeStatus approve(String approverId, BigDecimal dayLimitUsed) {
-
-        Approver approver = getApproverOnThisNode(approverId);
-
-        //节点设置的日累计直接对外支付金额 与 账户当日已用限额比较，如果大于，则可以直接结束节点，否则需要走原有逻辑
-        if(null != dayOutAmount && null != dayLimitUsed && (dayLimitUsed.add(transactionAmount)).compareTo(dayOutAmount) <= 0) {
-        	status = NodeStatus.END_FLOW;
-        }
-        else if (approver.couldDeal(transactionAmount)) {
-            status = NodeStatus.END_FLOW;
-        }
-        else if (++approvedCount == minApprovalCount) {
+    public NodeStatus approve() {
+       if (++approvedCount == minApprovalCount) {
             status = NodeStatus.END_NODE;
         }
         else {
@@ -101,28 +64,4 @@ public class FlowNode {
         log.debug("当前节点已经被驳回,{}", this);
     }
 
-    public NodeStatus preApproval(String approverId) {
-        Approver approver = getApproverOnThisNode(approverId);
-        if (approver.couldDeal(transactionAmount))
-            return NodeStatus.END_FLOW;
-        else if (approvedCount + 1 == minApprovalCount)
-            return NodeStatus.END_NODE;
-        else
-            return NodeStatus.CONTINUE;
-    }
-
-
-    private Approver getApproverOnThisNode(String approverId) {
-        return approverList.stream().filter(x -> x.getUserNo().equals(approverId)).findFirst().get();
-    }
-    
-    public boolean containUserNos(List<Long> userNos) {
-		for (Approver approver : approverList) {
-			long userNo = Long.parseLong(approver.getUserNo());
-			if(userNos.contains(userNo)) {
-				return true;
-			}
-		}
-		return false;
-	}
 }
